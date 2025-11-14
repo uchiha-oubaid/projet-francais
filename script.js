595f59727ca8ae4c13f83b3f040d91406fb85064
Oubaid Haji <uchihaoubaid@gmail.com>
2025-11-14 20:02:07 +0100

    initial setup

---
+++ script.js
@@ -0,0 +1,20 @@
+const discordBtn = document.getElementById("discord");
+const faceBtn = document.getElementById("facebook");
+const instaBtn = document.getElementById("instagram");
+const githuBtn = document.getElementById("github");
+
+discordBtn.addEventListener("click", () => {
+    window.open("https://discord.gg/DsDBFcyJ", "_blank");
+});
+
+faceBtn.addEventListener("click", () => {
+    window.open("https://www.facebook.com/obdahaji", "_blank");
+});
+
+instaBtn.addEventListener("click", () => {
+    window.open("https://www.instagram.com/oubaid_haji/", "_blank");
+});
+
+githuBtn.addEventListener("click", () => {
+    window.open("https://github.com/uchiha-oubaid", "_blank");
+});
