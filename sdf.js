c7b3e7b9065272cfe2137f6201ac8a012f35a94a
Oubaid Haji <uchihaoubaid@gmail.com>
2025-11-15 09:03:49 +0100

    fixing

--- sdf.js
+++ sdf.js
@@ -1,10 +1,15 @@
 const backHome = document.getElementById("back-to-home");
 const backEsc = document.getElementById("esclavage");
+const imgEsc = document.getElementById("images");
 
 backHome.addEventListener("click", () => { // for the home page
-    window.open("./index.html", "_blank");
+    window.open("./index.html", "_self");
 });
 
 backEsc.addEventListener("click", () => { // for the esclavage
-    window.open("./lesclavade.html", "_blank");
+    window.open("./lesclavade.html", "_self");
+});
+
+imgEsc.addEventListener("click", () => {
+    window.open("./images_sdf.html", "_self");
 });
